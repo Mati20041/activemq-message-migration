7740f871df01f9c00f88020f5245595132b7de57
mateusz.gizycki <mateusz.gizycki@ocado.com>
2017-10-18 17:06:05 +0200

    added optional second queue name

--- src/main/java/pl/mati/migration/Main.java
+++ src/main/java/pl/mati/migration/Main.java
@@ -2,8 +2,6 @@ package pl.mati.migration;
 
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.slf4j.Logger;
@@ -17,17 +15,18 @@ public class Main {
         CamelContext camelContext = new DefaultCamelContext();
 
         if (args.length < 3) {
-            System.out.println("camel-migraiton <firstBrokerUri> <secondBrokerUri> <queueName>");
+            System.out.println("camel-migraiton <firstBrokerUri> <secondBrokerUri> <queueName> <?secondQueueName>");
             return;
         }
 
         String firstBrokerUrl = args[0];
         String secondBrokerUrl = args[1];
-        String queueName = args[2];
+        String firstQueueName = args[2];
+        String secondQueueName = args.length == 4 ? args[3] : firstQueueName;
 
         LOG.info("first broker: {}" ,firstBrokerUrl);
         LOG.info("second broker: {}" ,secondBrokerUrl);
-        LOG.info("queue name: {}" ,queueName);
+        LOG.info("queue name: {}" ,firstQueueName);
 
         ActiveMQComponent firstBroker = ActiveMQComponent.activeMQComponent(firstBrokerUrl);
         ActiveMQComponent secondBroker = ActiveMQComponent.activeMQComponent(secondBrokerUrl);
@@ -38,12 +37,12 @@ public class Main {
         camelContext.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                errorHandler(deadLetterChannel("activemq1:deadmigration."+queueName)
+                errorHandler(deadLetterChannel("activemq1:deadmigration."+firstQueueName)
                         .useOriginalMessage());
 
-                from("activemq1:"+queueName)
+                from("activemq1:"+firstQueueName)
                         .log(body().toString())
-                        .to("activemq2:"+queueName);
+                        .to("activemq2:"+secondQueueName);
             }
         });
 
